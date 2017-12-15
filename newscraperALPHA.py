4ec6f819f14d08829d31f61ce2d8de4ad00590ac
Liam Dunbar <33041060+LiamDunbar@users.noreply.github.com>
2017-12-15 15:10:29 -0800

    Add files via upload

---
+++ newscraperALPHA.py
@@ -0,0 +1,60 @@
+#THIS IS STILL A WORK IN PROGRESS IT PROBABLY WONT WORK YET
+
+# import libraries
+import urllib2
+from bs4 import BeautifulSoup
+import requests
+from twilio.rest import Client
+import csv
+from datetime import datetime
+import time
+import re
+import sys
+
+accountsid = 'AC0be97adffcb60200330a039a8a886fb6'
+authtoken = '5eaa45f4c0ecca4f71947fb32b684743'
+twiliophonenumber = '+19713402015'
+myphonenumber = '+15035770930'
+
+try:
+	from bs4 import BeautifulSoup
+except Exception:
+	print "Unless you have BeautifulSoup downloaded, this program won't function correctly. Use 'pip install BeautifulSoup4' to download it." 
+	quit()
+
+def main():
+    time1 = raw_input("How long do you want to search for in seconds? ")
+    wait = raw_input("How long do you want to wait between searches in seconds? ")
+    time2 = float(time1)
+    while time2 > 1:
+        quote_page = raw_input("Paste URL Here:")
+        page = urllib2.urlopen(quote_page)
+        data = page.read()
+        datastring = str(data)
+        lengthB = len(datastring)
+        time = 0
+        soup = BeautifulSoup(page, 'html.parser')
+        price_box = soup.find('td', attrs={'id':'quotePrice'})
+        price = price_box.text
+        if time1 == 0:
+        		while True:
+        			page = urllib2.urlopen(quote_page)
+        			data = page.read()
+        			datastring = str(data)
+        			lengthA = len(datastring)
+        			if lengthB != lengthA:
+        				print price
+                        body= price
+                        Client = Client(accountsid, authtoken)
+                        Client.messages.create(
+                            body=body,
+                            to=myphone,
+                            from_=twiliophone
+                            )
+        with open('index.csv', 'a') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([price, datetime.now()])
+        time2 -= float(wait)
+        time.sleep(1)
+
+main()
