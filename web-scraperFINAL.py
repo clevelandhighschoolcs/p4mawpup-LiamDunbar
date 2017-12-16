d423b7cfd6c96d54c97c6bcaffc3ac91fd825d27
Liam Dunbar <33041060+LiamDunbar@users.noreply.github.com>
2017-12-15 19:17:10 -0800

    Adds Twilio Notification

--- web-scraperFINAL.py
+++ web-scraperFINAL.py
@@ -1,39 +1,58 @@
 # import libraries
 import urllib2
-
-#this code checks to make sure Beautiful Soup functions as planned. If not, the program stops instead of giving the user an error
-try:
-	from bs4 import BeautifulSoup
-except Exception:
-	print "Unless you have BeautifulSoup downloaded, this program won't function correctly. Use 'pip install BeautifulSoup4' to download it." 
-	quit()
-
+from bs4 import BeautifulSoup
+import requests
+from twilio.rest import Client
 import csv
 from datetime import datetime
 import time
 import re
 import sys
 
+accountsid = 'XXXXXXXXXXXX'
+authtoken = 'XXXXXXXXXXXXX'
+twiliophonenumber = '+1XXXXXXXX'
+myphonenumber = '+1XXXXXXXXX'
+
+try:
+	from bs4 import BeautifulSoup
+except Exception:
+	print "Unless you have BeautifulSoup downloaded, this program won't function correctly. Use 'pip install BeautifulSoup4' to download it." 
+	quit()
+
 def main():
     time1 = raw_input("How long do you want to search for in seconds? ")
     wait = raw_input("How long do you want to wait between searches in seconds? ")
     time2 = float(time1)
     while time2 > 1:
-        # specify the url
-        quote_page = 'https://www.investopedia.com/markets/stocks/intc/?ad=dirN&qo=investopediaSiteSearch&qsrc=0&o=40186'
-        # query the website and return the html to the variable 'page'
+        quote_page = raw_input("Paste URL Here:")
         page = urllib2.urlopen(quote_page)
-        # parse the html using beautiful soap and store in variable `soup`
+        data = page.read()
+        datastring = str(data)
+        lengthB = len(datastring)
+        time = 0
         soup = BeautifulSoup(page, 'html.parser')
-        # get the index price
         price_box = soup.find('td', attrs={'id':'quotePrice'})
-        price = price_box.text
-        print price
-        # open a csv file with append, so old data not be erased
-        with open('index.csv', 'a') as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerow([price, datetime.now()])
-        time2 -= float(wait)
-        time.sleep(1)
-	
+        price = price_box
+    if time1 == 0:
+        while True:
+        	page = urllib2.urlopen(quote_page)
+        	data = page.read()
+        	datastring = str(data)
+        	lengthA = len(datastring)
+        	if lengthB != lengthA:
+        		print price
+                body= price
+                Client = Client(accountsid, authtoken)
+                Client.messages.create(
+                body=body,
+                to=myphone,
+                from_=twiliophone
+                            )
+	with open('index.csv', 'a') as csv_file:
+			writer = csv.writer(csv_file)
+			writer.writerow([price, datetime.now()])
+			time2 -= float(wait)
+			time.sleep(1)
+
 main()
